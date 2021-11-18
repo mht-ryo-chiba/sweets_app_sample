db971b58abc45071bac3041912e25a286594577f
mht-ryo-chiba <r.chiba@mht-code.com>
2021-11-19 04:51:18 +0900

    shopDetailWidgetの作成

--- lib/ui/templates/app_parts_list_template.dart
+++ lib/ui/templates/app_parts_list_template.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sweets_app_sample/config/app_routes.dart';
 import 'package:sweets_app_sample/ui/atoms/app_text_styles.dart';
 import 'package:sweets_app_sample/ui/organisms/comment_detail_widget.dart';
+import 'package:sweets_app_sample/ui/organisms/shop_detail_widget.dart';
 import 'package:sweets_app_sample/ui/organisms/shop_list_widget.dart';
 
 class AppPartsListTemplate extends StatelessWidget {
@@ -32,6 +33,26 @@ class AppPartsListTemplate extends StatelessWidget {
               shopName: '長めの店舗名です長めの店舗名です長めの店舗名です長めの店舗名です長めの店舗名です',
               shopAddress: '長めの住所です長めの住所です長めの住所です長めの住所です長めの住所です',
               tapFunction: () {},
+            ),
+            const Divider(color: Colors.black),
+            const SizedBox(height: 16),
+            const Text(
+              'CommentDetailWidget',
+              style: AppTextStyles.titleText,
+            ),
+            const ShopDetailWidget(
+              shopName: '店舗名テキスト',
+              shopOpenTime: '10:00 - 19:00',
+              shopPhoneNo: '01-2345-6789',
+              shopAddress: '東京都千代田区1-1-1',
+              src: 'https://placehold.jp/500x500.png',
+            ),
+            const ShopDetailWidget(
+              shopName: '長めの店舗名テキスト長めの店舗名テキスト長めの店舗名テキスト',
+              shopOpenTime: '不定期',
+              shopPhoneNo: '-',
+              shopAddress: '-',
+              src: 'https://placehold.jp/500x500.png',
             ),
             const Divider(color: Colors.black),
             const SizedBox(height: 16),
